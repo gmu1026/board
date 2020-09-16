7d2c847386ff95612eeeaaac85b056b7e3d120ea
gmu1026 <gmu01026@gmail.com>
2020-09-16 15:28:40 +0900

    Initial commit

---
+++ src/test/java/com/cproduction/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cproduction.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
